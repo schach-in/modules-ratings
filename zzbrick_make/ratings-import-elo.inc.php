188f235aeb8fd49ee60c021fee89f6e3488c21eb
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-11 23:55:23 +0200

    ratings import elo: code style return directly if no handle, save lines and imports for later use

--- zzbrick_make/ratings-import-elo.inc.php
+++ zzbrick_make/ratings-import-elo.inc.php
@@ -37,39 +37,44 @@ function mod_ratings_make_ratings_import_elo($params) {
 
 	$file = $params[0].'/players_list_foa.txt';
 	$handle = fopen($file, 'r');
-	$i = 0;
-	if ($handle) {
-		while (($line = fgets($handle, 4096)) !== false) {
-			if (!$i) {
-				// not a standard title line
-				$line = str_replace('ID Number', 'ID_Number', $line);
-				preg_match_all('~([-\w]+)\s+~', $line, $matches);
-				$fields = $matches[1];
-				$pos = 0;
-				foreach ($matches[0] as $index => $field) {
-					$lengths[$index] = strlen($field);
-					$start[$index] = $pos;
-					$pos += strlen($field);
-				}
-			} else {
-				$fieldnames = [];
-				$values = [];
-				foreach ($fields as $index => $field) {
-					$value = trim(substr($line, $start[$index], $lengths[$index]));
-					if (!$value) continue;
-					if ($field === 'B-day' AND $value === '0000') continue;
-					$fieldnames[] = $field_names[$field];
-					$values[] = is_numeric($value) ? $value : sprintf('"%s"', $value);
-				}
-				if (count($values) > 1) {
-					// sometimes, FIDE exports have errors
-					$sql = 'INSERT INTO fide_players (%s) VALUES (%s)';
-					$sql = sprintf($sql, implode(',', $fieldnames), implode(',', $values));
-					wrap_db_query($sql);
-				}
+	if (!$handle)
+		return [wrap_text('Unable to open rating file %s', ['values' => [$file]])];
+
+	$data = [
+		'lines' => 0,
+		'imports' => 0
+	];
+	while (($line = fgets($handle, 4096)) !== false) {
+		if (!$data['lines']) {
+			// not a standard title line
+			$line = str_replace('ID Number', 'ID_Number', $line);
+			preg_match_all('~([-\w]+)\s+~', $line, $matches);
+			$fields = $matches[1];
+			$pos = 0;
+			foreach ($matches[0] as $index => $field) {
+				$lengths[$index] = strlen($field);
+				$start[$index] = $pos;
+				$pos += strlen($field);
 			}
-			$i++;
+		} else {
+			$fieldnames = [];
+			$values = [];
+			foreach ($fields as $index => $field) {
+				$value = trim(substr($line, $start[$index], $lengths[$index]));
+				if (!$value) continue;
+				if ($field === 'B-day' AND $value === '0000') continue;
+				$fieldnames[] = $field_names[$field];
+				$values[] = is_numeric($value) ? $value : sprintf('"%s"', $value);
+			}
+			if (count($values) > 1) {
+				// sometimes, FIDE exports have errors
+				$sql = 'INSERT INTO fide_players (%s) VALUES (%s)';
+				$sql = sprintf($sql, implode(',', $fieldnames), implode(',', $values));
+				wrap_db_query($sql);
+				$data['imports']++;
+			}
 		}
+		$data['lines']++;
 	}
     fclose($handle);
 	unlink($file);
